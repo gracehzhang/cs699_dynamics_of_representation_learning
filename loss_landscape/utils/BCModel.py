aaffa6499f32e6432441929e69d8b7ddb5dc2852
Sumedh Anand Sontakke <ssontakk@iGpu22.usc.edu>
2022-02-08 11:25:22 -0800

    BC changed

--- loss_landscape/utils/BCModel.py
+++ loss_landscape/utils/BCModel.py
@@ -1,13 +1,15 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import gym
+import numpy as np
 # model definition
 class MLP(torch.nn.Module):
     # define model elements
-    def __init__(self, env, n_inputs, discrete=False):
+    def __init__(self, env):
         super(MLP, self).__init__()
         self.env = env
-        state_dim = env.observation_space.shape[0]
+        # state_dim = env.observation_space.shape[0]
         # check if environment action space is discrete
         if isinstance(env.action_space, gym.spaces.discrete.Discrete):
             action_dim = env.action_space.n
@@ -16,12 +18,20 @@ class MLP(torch.nn.Module):
             action_dim = env.action_space.shape[0]
             self.action_type = "continuous"
 
+
+        if env.observation_space.shape == None:
+            # print()
+            state_dim = np.prod(env.observation_space['image'].shape) 
+        else:
+            state_dim = env.observation_space.shape[0]
         self.layer1 = nn.Linear(state_dim, 256)
-        self.layer2 = Linear(256, action_dim)
+        self.layer2 = nn.Linear(256, action_dim)
         self.activation = nn.ReLU()
  
     # forward propagate input
     def forward(self, X):
+        if self.action_type == "discrete":
+            X = torch.flatten(X, start_dim=1)
         X = self.layer1(X)
         X = self.activation(X)
         X = self.layer2(X)
@@ -35,13 +45,19 @@ class MLP(torch.nn.Module):
             out = self.forward(X)
 
     def compute_loss(self, inputs):
-        out = self.forward(inputs['obs'])
+        out = self.forward(inputs['observations'])
+        total = 0
+        correct = 0
         if self.action_type =="discrete":
-            loss = nn.BCEWithLogitsLoss()
-            return loss(out, inputs['actions'])
+            loss = nn.CrossEntropyLoss()
+            _, predicted = torch.max(out.data, 1)
+            total += inputs['observations'].size(0)
+            correct += (predicted == inputs['actions']).sum().item()
+            # print(type(inputs['actions'][0]))
+            return loss(out, inputs['actions'].long()), 100 * correct / total
         else:
             loss = nn.MSELoss()
-            return loss(out, inputs['actions'])
+            return loss(out, inputs['actions']), None
 
 
 
@@ -49,3 +65,6 @@ class MLP(torch.nn.Module):
 def get_model(model_string):
     if model_string == "BC":
         return MLP
+
+
+
