9e1574acdd3e55a8c75b3aff44da953dc2099c49
Jesse Zhang <jessezhang5@gmail.com>
2022-03-31 17:02:09 -0700

    SWAAA

--- loss_landscape/train.py
+++ loss_landscape/train.py
@@ -241,8 +241,8 @@ if __name__ == "__main__":
 
     swa_model = AveragedModel(model)
     scheduler = CosineAnnealingLR(optimizer, T_max=100)
-    swa_start = 95
-    swa_scheduler = SWALR(optimizer, swa_lr=0.05)
+    swa_start = 75
+    swa_scheduler = SWALR(optimizer, swa_lr=1e-3)
 
     # training loop
     # we pass flattened gradients to the FrequentDirectionAccountant before clearing the grad buffer
