04767db6533f3c704f72f29c9c4fa763092bc8e1
Jesse Zhang <jessezhang5@gmail.com>
2022-04-22 18:49:12 -0700

    added UCB runs hit

--- loss_landscape/utils/UCB.py
+++ loss_landscape/utils/UCB.py
@@ -1,8 +1,36 @@
-from importlib.metadata import requires
-from turtle import forward
+from copyreg import pickle
 import numpy as np
 import torch.nn as nn
 import torch
+import argparse
+from BCModel import MLPEnsemble
+import d4rl
+import tqdm
+import gym
+import os
+
+
+def simple_evaluate_policy(model, env, num_episodes, device):
+    rews = []
+
+    pbar = tqdm(range(num_episodes))
+    for i in pbar:
+        pbar.set_description("Evaluating ...")
+        eval_env = env
+        ob = eval_env.reset()
+
+        while True:
+            ### ASDF make sure this works for BC
+            if isinstance(ob, dict):
+                ob = ob["image"]
+            ob = torch.Tensor(np.expand_dims(ob, axis=0)).to(device)
+            ac = model.compute_action(ob)[0].cpu().detach().numpy()
+            ob, rew, done, _ = eval_env.step(ac)
+            rews.append(rew)
+
+            if done:
+                break
+    return np.sum(rews) / num_episodes
 
 
 class UCBPolicy(nn.Module):
@@ -44,3 +72,37 @@ class UCBPolicy(nn.Module):
         self.experimental_means[self.currently_picked_arm] += (
             reward - self.experimental_means[self.currently_picked_arm]
         ) / self.num_times_picked[self.currently_picked_arm]
+
+
+def main(args):
+    cum_rewards = []
+    env = gym.make(args.env)
+    arms = MLPEnsemble(env, args.load_dir)
+    UCB = UCBPolicy(arms, args.epsilon_greedy).to(args.device)
+    for episode in args.n_episodes:
+        UCB.pick_new_arm(episode)
+        reward = simple_evaluate_policy(UCB, env, 1, args.device)
+        if episode % 10 == 0:
+            eval_reward = simple_evaluate_policy(UCB, env, 20, args.device)
+            print(f"Episode: {episode}, Eval reward: {eval_reward}")
+            cum_rewards.append((episode, eval_reward))
+        UCB.update_experimental_means(reward)
+    save_dir = os.path.join(
+        args.save_dir, args.env, f"UCB_eps-{args.epsilon_greedy}_run-{args.run_number}"
+    )
+    os.makedirs(save_dir, exist_ok=True)
+    torch.save(UCB.state_dict(), os.path.join(save_dir, "model.pt"))
+    pickle.dump(cum_rewards, open(os.path.join(save_dir, "cum_rewards.pkl"), "wb"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_episodes", required=False, type=int, default=4000)
+    parser.add_argument("--load_dir", required=True, type=str, default=None)
+    parser.add_argument("--env", required=True, type=str, default=None)
+    parser.add_argument("--epsilon_greedy", required=False, type=bool, default=False)
+    parser.add_argument("--save_dir", required=True, type=str, default=None)
+    parser.add_argument("--run_number", required=True, type=int, default=None)
+    parser.add_argument("--device", required=False, type=str, default="0")
+    args = parser.parse_args()
+    main(args)
